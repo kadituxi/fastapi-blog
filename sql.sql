d46f4cfa0bd049ca502427640aa58492670aad1c
kadituxi <luismanuelmuhele@gmail.com>
2026-05-05 14:47:53 +0100

    setup: basic project configs

--- sql.sql
+++
@@ -1,5 +0,0 @@
--- Active: 1772248809767@@127.0.0.1@5432@orders_api
-
-select * from public.user;
-
-delete from public.user where id = 2;
--- src/app.py
+++ src/app.py
@@ -1,22 +1,12 @@
 from fastapi import FastAPI
-import uvicorn
-
-from routes import products_routes, users_routes, orders_routes
-# from db.db import Base, engine
-# from models import order_model, product_model, user_model
 
 app = FastAPI(
-    title="Kadituxi Store API",
-    description="Kadituxi Store - API for orders management.",
+    title="FastAPI Blog",
+    description="My first complete project using FastAPI",
     version="1.0",
 )
 
-app.include_router(router=users_routes.users_router, prefix="/api/v1/users")
-app.include_router(router=products_routes.products_router, prefix="/api/v1/products")
-app.include_router(router=orders_routes.orders_router, prefix="/api/v1/orders")
 
-
-if __name__ == "__main__":
-    # Base.metadata.create_all(engine)
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
-
+@app.get("/helth")
+def get_api_status():
+    return {"status": "Helth"}
